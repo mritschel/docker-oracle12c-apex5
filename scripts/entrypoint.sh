7ac8e82fbb3dc45b13df612cee2216b4f101bc5b
mritschel <martin@ritschel-privat.de>
2016-11-22 12:42:02 +0100

    New version
    The new version is based on the
    https://hub.docker.com/r/mritschel/oraclebase/ image with Oracle
    Enterprise Linux 7.1 and Oracle Database 12c R1

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-source /scripts/colorecho
+source $SCRIPTS_HOME/colorecho
 
 # Add oracle to path
-export PATH=${ORACLE_HOME}/bin:$PATH
+export PATH=$ORACLE_HOME/bin:$PATH
 if grep -q "PATH" ~/.bashrc
 then
     echo "Found PATH definition in ~/.bashrc"
@@ -13,20 +13,9 @@ else
 	printf "\nPATH=${PATH}\n" >> ~/.bashrc
 fi
 
-echo_yellow "Starting jenkins on 9090"
-echo_yellow "---------------------------------------------------------------------------"
-java -jar /opt/jenkins.war --httpPort=9090 > /var/log/jenkis.log 2>&1 &
-echo_yellow "Jenkins console initialized. Please visit"
-echo_yellow "   - http://localhost:9090"
-echo_yellow "\n"
-echo_yellow "Jenkins initial setup is required. An admin user has been created and a password generated."
-echo_yellow "This may also be found at: /jenkins/secrets/initialAdminPassword"
-echo_yellow "\n"
-echo_yellow "---------------------------------------------------------------------------"
-echo "\n \n \n"
 echo_yellow  "Starting listener and database"
 echo_yellow "---------------------------------------------------------------------------"
-su oracle -c '/scripts/startup.sh database'
+$SCRIPTS_HOME/startup.sh database
 echo_yellow "Database and Web management console initialized. Please visit"
 echo_yellow "   - http://localhost:8080/em"
 echo_yellow "   - http://localhost:8080/apex"
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 ##########################################################################
 #  Author   M. Ritschel 
 #           Trivadis GmbH Hamburg
-#  Created: 28.09.2016 
+#  Created: 22.11.2016 
 #  Base-information 
 #  ------------------------
 # Installation-Script for the Trivadis docker images
@@ -10,65 +10,56 @@
 ##########################################################################
 set -e
 
-mkdir -p /entrypoint-initdb.d
+
 # Read Hostname
 HOSTNAME=$(cat /etc/hostname)
 
-# Prevent owner issues on mounted folders
-chown -R oracle:dba /u01/app/oracle
-rm -f /u01/app/oracle/product
-ln -s /u01/app/oracle-product /u01/app/oracle/product
-
-#Run Oracle root scripts
-/u01/app/oraInventory/orainstRoot.sh > /dev/null 2>&1
-echo | /u01/app/oracle/product/12.1.0/xe/root.sh > /dev/null 2>&1 || true
-
+# Check and create tnsnames.ora
 echo "Checking tnsnames.ora"
 if [ -f "${ORACLE_HOME}/network/admin/tnsnames.ora" ] 
 then 
 	echo "tnsnames.ora found." 
-	rm -f ${ORACLE_HOME}/network/admin/tnsnames.ora
+	rm -f $ORACLE_HOME/network/admin/tnsnames.ora
 fi 
 echo "Creating tnsnames.ora"  
-printf "${ORACLE_SID} = 
+printf "$ORACLE_SID = 
    (DESCRIPTION = 
       (ADDRESS = (PROTOCOL = TCP)
       (HOST = $HOSTNAME) 
       (PORT = 1521)) 
       (CONNECT_DATA = 
-         (SERVICE_NAME = ${SERVICE_NAME})
+         (SERVICE_NAME = $SERVICE_NAME)
       )
-   )\n" > ${ORACLE_HOME}/network/admin/tnsnames.ora 
-chown -R oracle:dba ${ORACLE_HOME}/network/admin/tnsnames.ora
+   )\n" > $ORACLE_HOME/network/admin/tnsnames.ora 
+chown -R oracle:dba $ORACLE_HOME/network/admin/tnsnames.ora
+ 
+ 
+# create database
+echo "create database $ORACLE_SID"
+$ORACLE_HOME/bin/dbca -silent -createDatabase -templateName General_Purpose.dbc -gdbname xe.oracle.docker -sid xe -responseFile NO_VALUE -characterSet AL32UTF8 -totalMemory $DBCA_TOTAL_MEMORY -emConfiguration LOCAL -pdbAdminPassword oracle -sysPassword oracle -systemPassword oracle
 
-# Start database installation
-echo "Initializing database."
-mv /u01/app/oracle-product/12.1.0/xe/dbs /u01/app/oracle/dbs
-ln -s /u01/app/oracle/dbs /u01/app/oracle-product/12.1.0/xe/dbs
+# Apex remove Version 4.x and install Version 5.x
 
-#create DB for SID: xe
-su oracle -c "$ORACLE_HOME/bin/dbca -silent -createDatabase -templateName General_Purpose.dbc -gdbname xe.oracle.docker -sid xe -responseFile NO_VALUE -characterSet AL32UTF8 -totalMemory $DBCA_TOTAL_MEMORY -emConfiguration LOCAL -pdbAdminPassword oracle -sysPassword oracle -systemPassword oracle"
+echo "Rremove old Apex Version" 
+cd $ORACLE_HOME
+echo -e "$ORACLE_HOME\n\n" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @./apex/apxremov.sql > /dev/null
 
-#move the apex instalations files to ${ORACLE_HOME}
-echo "Move the apex 5.0.3 instalations files to oracle_home"
-mv ${ORACLE_HOME}/apex ${ORACLE_HOME}/apex_old
-mv ${INSTALL_HOME}/apex ${ORACLE_HOME}
-chown -R oracle:dba ${ORACLE_HOME}/apex
-chmod -R 775 ${ORACLE_HOME}/apex
+echo "Move the Apex 5.0.3 instalations Files to $ORACLE_HOME"
+rm -fr $ORACLE_HOME/apex 
+cp -r $INSTALL_HOME/apex $ORACLE_HOME 
 
-#config Apex console
-echo "Configuring Apex console"
-cd $ORACLE_HOME/apex
-su oracle -c 'echo -e "${PASS}\n8060" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @apxconf > /dev/null'
-su oracle -c 'echo -e "${ORACLE_HOME}\n\n" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @apex_epg_config_core.sql > /dev/null'
-su oracle -c 'echo -e "ALTER USER ANONYMOUS ACCOUNT UNLOCK;" | $ORACLE_HOME/bin/sqlplus -S / as sysdba > /dev/null'
-su oracle -c 'echo -e "${ORACLE_HOME}\n\n" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @apxxepwd ${APEX_PASS} > /dev/null'
+
+echo "Install and Configuration Apex console"
+echo -e "SYSAUX\n SYSAUX\n TEMP\n /i/" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @apexins > /dev/null
+echo -e "$ORACLE_HOME\n\n" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @apxldimg $ORACLE_HOME > /dev/null
+echo -e "ALTER USER ANONYMOUS ACCOUNT UNLOCK;" | $ORACLE_HOME/bin/sqlplus -S / as sysdba > /dev/null
+echo -e "$ORACLE_HOME\n\n" | $ORACLE_HOME/bin/sqlplus -S / as sysdba @apxxepwd $APEX_PASS > /dev/null
+
 
 echo "Set NAMES.DEFAULT_DOMAIN for the sqlnet..."
-# sqlnet modify 
 STRSEARCH="NAMES.DEFAULT_DOMAIN"
 STRREPLACE="#NAMES.DEFAULT_DOMAIN"
-find "${ORACLE_HOME}/network/admin" -type f -name '*.ora' -print | while read i
+find "$ORACLE_HOME/network/admin" -type f -name '*.ora' -print | while read i
 do
    cp "$i" "$i.tmp"
    if [ -f "$i.tmp" ]; then
@@ -84,7 +75,7 @@ do
    fi
 done
 
-# clearing
+# Clearing
 echo "Clearing"
 rm -f /scripts/install.sh
 rm -fr $INSTALL_HOME/* 
